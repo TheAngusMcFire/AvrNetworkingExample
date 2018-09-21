dd61738185d30514071ad7258706287bcb8a0564
Christian Rieger <christian.rieger@student.tugraz.at>
2018-09-21 23:26:17 +0200

    implemted uart driver

---
+++ hardware_abstraction_layer/atmega32/device.h
@@ -0,0 +1,5 @@
+#include <avr/io.h>
+
+#ifndef F_CPU
+#define F_CPU 16000000lu
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,14 @@
-
+#include <uart.h>
+#include <udp.h>
 
 int main()
 {
+    uartWriteString("Hello this is the Netwok code");
+
+    while(1)
+    {
+
+    }
 
     return 0;
 }
